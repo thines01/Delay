64636df5e51040d678c673e32d1d822e9f4c8fb2
thines01 <thines01@yahoo.com>
2023-07-08 13:14:52 -0500

    Add project files.

---
+++ Delay.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace Delay
+{
+   public static class CDelay
+   {
+      public static void Delay(uint uintNumSeconds)
+      {
+         Thread.Sleep((int)(uintNumSeconds * 1000)); // delay
+      }
+   }
+}
